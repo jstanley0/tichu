dcd137301c7d6b356561d706f3a18ae91e7cebc2
Jeremy Stanley <jeremy@instructure.com>
2020-07-14 10:45:15 -0600

    some sizing nonsense

--- tichu/public/Game.js
+++ tichu/public/Game.js
@@ -1,6 +1,6 @@
 function Game({game_id, player_id}) {
   const { useState, useEffect, useRef } = React
-  const { Container, Grid, Button, CircularProgress } = MaterialUI
+  const { Container, Grid, Box, Typography, Button, CircularProgress } = MaterialUI
 
   const [ socket, setSocket ] = useState()
   const [ gameState, setGameState ] = useState({"state":"connecting"})
@@ -28,22 +28,34 @@ function Game({game_id, player_id}) {
   }
 
   return <Container>
-    <Grid container spacing='2'>
-     <Grid item xs={12}>
+    <Grid container spacing='2' className='full_height'>
+      <Grid item xs={3}>
+        <Box height="25%"/>
+      </Grid>
+      <Grid item xs={6}>
        <Player data={gameState.players[2]} orientation='h'/>
-     </Grid>
-     <Grid item xs={3}>
-       <Player data={gameState.players[1]} orientation='v'/>
-     </Grid>
-     <Grid item xs={6}>
-       <History data={history}/>
-     </Grid>
-     <Grid item xs={3}>
-       <Player data={gameState.players[3]} orientation='v'/>
-     </Grid>
-     <Grid item xs={12}>
+      </Grid>
+      <Grid item xs={3}>
+        <Typography align='right'>Terrible Tichu</Typography>
+        <Typography variant='h4' align='right' component='h1'>{ gameState.id }</Typography>
+      </Grid>
+      <Grid item xs={3}>
+       <Player data={gameState.players[1]} orientation='v' align='left'/>
+      </Grid>
+      <Grid item xs={6}>
+        <Box height='50%'>
+          <History data={history}/>
+        </Box>
+      </Grid>
+      <Grid item xs={3}>
+       <Player data={gameState.players[3]} orientation='v' align='right'/>
+      </Grid>
+      <Grid item xs={3}>
+        <Box height="25%"/>
+      </Grid>
+      <Grid item xs={6}>
        <Player data={gameState.players[0]} orientation='h'/>
-     </Grid>
+      </Grid>
     </Grid>
   </Container>
 }
--- tichu/public/Player.js
+++ tichu/public/Player.js
@@ -1,13 +1,11 @@
-function Player({data, orientation}) {
-  const { Grid, CircularProgress } = MaterialUI
-
-  if (!data) {
-    return <CircularProgress/>
-  }
+function Player({data, orientation, align}) {
+  const { Grid, Typography, CircularProgress } = MaterialUI
 
   return <Grid container>
     <Grid item xs={12}>
-      { data.name }
+      <Typography variant='h6' component='h2' align={orientation == 'v' ? align : 'center'}>
+        { data ? data.name : 'Waiting...' }
+      </Typography>
     </Grid>
   </Grid>
 }
