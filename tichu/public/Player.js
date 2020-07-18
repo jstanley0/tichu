a1409953dd621594dfd190f454870c7ecfac09de
Jeremy Stanley <jeremy@instructure.com>
2020-07-18 14:08:45 -0600

    state hooks are acting funny

--- tichu/public/Player.js
+++ tichu/public/Player.js
@@ -29,13 +29,155 @@ function PlayerInfo({data}) {
     </div>
 }
 
+function insertIntoArray(array, value, index) {
+  const a = Array.from(array)
+  a.splice(index, 0, value)
+  return a
+}
+
+function removeFromArray(array, index) {
+  const a = Array.from(array)
+  a.splice(index, 1)
+  return a
+}
+
+function reorderArray(array, source_index, dest_index) {
+  const a = Array.from(array)
+  const [removed] = a.splice(source_index, 1)
+  a.splice(dest_index, 0, removed)
+  return a
+}
+
 function Player0({gameState}) {
   const { Box } = MaterialUI
-  const { useCallback } = React
+  const { useState, useCallback } = React
   const { DragDropContext } = ReactBeautifulDnd
 
-  const onDragEnd = useCallback((result) => {
+  const [ hand, setHand ] = useState(gameState.players[0].hand)
+  const [ cards, setCards ] = useState([])
+  const [ card0, setCard0 ] = useState('')
+  const [ card1, setCard1 ] = useState('')
+  const [ card2, setCard2 ] = useState('')
 
+  const onDragEnd = useCallback(({source, destination, draggableId}) => {
+    if (!destination) {
+      return
+    }
+    console.log(arguments)
+    // reorder
+    if (destination.droppableId === source.droppableId) {
+      switch(destination.droppableId) {
+        case 'hand':
+          setHand(reorderArray(hand, source.index, destination.index))
+          break
+        case 'cards':
+          setCards(reorderArray(cards, source.index, destination.index))
+          break
+      }
+      return
+    }
+
+    // hoo boy, this is in dire need of refactoring
+    switch(destination.droppableId) {
+      case 'hand':
+        switch(source.droppableId) {
+          case 'playTarget':
+            setCards(removeFromArray(cards, source.index))
+            break
+          case 'passLeft':
+            setCard0('')
+            break
+          case 'passAcross':
+            setCard1('')
+            break
+          case 'passRight':
+            setCard2('')
+            break
+        }
+        setHand(insertIntoArray(hand, draggableId, destination.index))
+        break
+      case 'playTarget':
+        switch(source.droppableId) {
+          case 'hand':
+            setHand(removeFromArray(hand, source.index))
+            break
+          case 'passLeft':
+            setCard0('')
+            break
+          case 'passAcross':
+            setCard1('')
+            break
+          case 'passRight':
+            setCard2('')
+            break
+        }
+        setCards(insertIntoArray(cards, draggableId, destination.index))
+        break
+      case 'passLeft':
+        switch(source.droppableId) {
+          case 'hand':
+            setHand(removeFromArray(hand, source.index))
+            break
+          case 'playTarget':
+            setCards(removeFromArray(cards, source.index))
+            break
+          case 'passAcross':
+            if (card0) {
+              setCard1(card0)
+            }
+            break
+          case 'passRight':
+            if (card0) {
+              setCard2(card0)
+            }
+            break
+        }
+        setCard0(draggableId)
+        break
+      case 'passAcross':
+        switch(source.droppableId) {
+          case 'hand':
+            setHand(removeFromArray(hand, source.index))
+            break
+          case 'playTarget':
+            setCards(removeFromArray(cards, source.index))
+            break
+          case 'passLeft':
+            if (card1) {
+              setCard0(card1)
+            }
+            break
+          case 'passRight':
+            if (card1) {
+              setCard2(card1)
+            }
+            break
+        }
+        setCard1(draggableId)
+        break
+      case 'passRight':
+        switch(source.droppableId) {
+          case 'hand':
+            setHand(removeFromArray(hand, source.index))
+            break
+          case 'playTarget':
+            setCards(removeFromArray(cards, source.index))
+            break
+          case 'passLeft':
+            if (card2) {
+              setCard0(card2)
+            }
+            break
+          case 'passAcross':
+            if (card2) {
+              setCard1(card2)
+            }
+            break
+        }
+        setCard2(draggableId)
+        break
+    }
+    console.log(result)
   }, [])
 
   return <div style={{display: 'flex', alignItems: 'flex-end'}}>
@@ -44,26 +186,28 @@ function Player0({gameState}) {
     <Box width={5} height={5}/>
     <DragDropContext onDragEnd={onDragEnd}>
       <div style={{display: 'flex', flexDirection: 'column-reverse'}}>
-        <Hand0 gameState={gameState}/>
-        { gameState.state === 'passing' && !gameState.passed_cards ?
-          <PassTarget/> : (gameState.state == 'playing' ?  <PlayTarget/> : null) }
+        <Hand0 hand={hand}/>
+        { gameState.state === 'passing' ?
+          (gameState.passed_cards ?
+            <PassSplay vertical={false} align='left'/>
+            : (gameState.players[0].hand.size == 14) ?
+              <PassTarget card0={card0} card1={card1} card2={card2}/>
+              : null)
+          : (gameState.state == 'playing' ?  <PlayTarget cards={cards}/> : null) }
       </div>
     </DragDropContext>
     <div style={{flexGrow: 1}}/>
   </div>
 }
 
-function Hand0({gameState}) {
-  const { useState } = React
+function Hand0({hand}) {
   const { Draggable, Droppable } = ReactBeautifulDnd
-
-  const [ hand, setHand ] = useState(gameState.players[0].hand)
 
   return <Droppable droppableId='hand' direction='horizontal'>
     {(provided, snapshot) => (
       <div ref={provided.innerRef} {...provided.droppableProps} style={{width: 14*70, height: 90, display: 'flex'}} className='hand0'>
         {hand.map((card, index) => (
-          <Draggable draggableId={card} index={index}>
+          <Draggable draggableId={card} index={index} key={card}>
             {(provided, snapshot) => (
               <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
                 <FaceCard key={card} card={card}/>
@@ -78,46 +222,56 @@ function Hand0({gameState}) {
   </Droppable>
 }
 
-function PassTarget({}) {
-  const { Droppable } = ReactBeautifulDnd
+function PassHolder({droppableId, caption, card})
+{
+  const { Draggable, Droppable } = ReactBeautifulDnd
+
+  return  <Droppable droppableId={droppableId} direction='horizontal'>
+      {(provided, snapshot) => (
+        <div ref={provided.innerRef} {...provided.droppableProps} style={{width: 70, height: 90}} className='passTarget'>
+          {caption}
+          {card && (<Draggable draggableId={card} index={0}>
+            {(provided, snapshot) => (
+              <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                <FaceCard key={card}/>
+                {provided.placeholder}
+              </div>
+            )}
+          </Draggable>)}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+}
+
+function PassTarget({card0, card1, card2}) {
   const { Box } = MaterialUI
   return <div style={{display: 'flex', margin: 5}}>
     <div style={{flexGrow: 1}}/>
-    <Droppable droppableId='passLeft' direction='horizontal'>
-      {(provided, snapshot) => (
-        <div ref={provided.innerRef} {...provided.droppableProps} style={{width: 70, height: 90}} className='passTarget'>
-          &#x2190;
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
+    <PassHolder card={card0} droppableId='passLeft' caption="&#x2190;"/>
     <Box width={10}/>
-    <Droppable droppableId='passAcross' direction='horizontal'>
-      {(provided, snapshot) => (
-        <div ref={provided.innerRef} {...provided.droppableProps} style={{width: 70, height: 90}} className='passTarget'>
-          &#x2191;
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
+    <PassHolder card={card1} droppableId='passAcross' caption="&#x2190;"/>
     <Box width={10}/>
-    <Droppable droppableId='passRight' direction='horizontal'>
-      {(provided, snapshot) => (
-        <div ref={provided.innerRef} {...provided.droppableProps} style={{width: 70, height: 90}} className='passTarget'>
-          &#x2192;
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
+    <PassHolder card={card2} droppableId='passRight' caption="&#x2190;"/>
     <div style={{flexGrow: 1}}/>
   </div>
 }
 
-function PlayTarget({}) {
-  const { Droppable } = ReactBeautifulDnd
+function PlayTarget({cards}) {
+  const { Draggable, Droppable } = ReactBeautifulDnd
   return <Droppable droppableId='playTarget' direction='horizontal'>
     {(provided, snapshot) => (
       <div ref={provided.innerRef} {...provided.droppableProps} style={{width: 14*70, height: 90, margin: 5}} className='playTarget'>
+        {cards.map((card, index) => (
+          <Draggable draggableId={card} index={index}>
+            {(provided, snapshot) => (
+              <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                <FaceCard key={card} card={card}/>
+                {provided.placeholder}
+              </div>
+            )}
+          </Draggable>
+        ))}
         {provided.placeholder}
       </div>
     )}
